8da69e66a1801249dede1e502fac1060c0de4d13
GeekZeke <zekeellis1@gmail.com>
2026-03-05 23:04:44 -0700

    added list assignment

---
+++ LISTTHING/Learning05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 3, 5));
+        shapes.Add(new Circle("Green", 2.5));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
+    }
+}
